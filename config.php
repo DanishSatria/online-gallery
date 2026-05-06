4e5273da2b237c045f61bbba412696861af4dee1
DanishSatria <danish_indie_33rpl@student.smktelkom-mlg.sch.id>
2026-05-06 08:46:20 +0700

    update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = 'localhost'; // atau alamat VM Anda
 $user = 'admin123';      // sesuaikan dengan user MariaDB
 $pass = 'admin123';          // password MariaDB Anda
-$db   = 'db_gallery';
+$db   = 'galllery_db';          // nama database yang sudah Anda buat
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
